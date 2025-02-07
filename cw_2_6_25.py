00ef3fb06b1c82e82f1446fb8b7b4718bc46a4a5
Xavier <xavierujauregui@gmail.com>
2025-02-06 21:44:00 -0800

    Challenge and solution for Codewars on 2/6/25

---
+++ cw_2_6_25.py
@@ -0,0 +1,10 @@
+# Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+
+
+def opposite(number): 
+  # your solution here
+    x = -number # This line muliplies argument by -1
+    return x
+
+opposite(10)
