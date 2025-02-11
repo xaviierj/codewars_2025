0b9a89ff3e5fe1af1dc2a9c151b4a02f707198ed
Xavier <xavierujauregui@gmail.com>
2025-02-10 22:11:48 -0800

    Challenge and solution for Codewars 2/10/25

---
+++ cw_2_10_25.py
@@ -0,0 +1,14 @@
+'''Task
+Create these three variables and store the appropriate values using the table above:
+la_liga_goals
+champions_league_goals
+copa_del_rey_goals
+Create a fourth variable named total_goals that stores the sum of all of Messi's goals for this year.'''
+
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
+
+print(total_goals)
